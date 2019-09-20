45f0c19d5e92acb1b905b9b2de3b7f6c1bee49fa
shayaantx <shayaantx@gmail.com>
2019-09-20 00:29:31 -0400

    Update upload-release.sh

--- upload-release.sh
+++ upload-release.sh
@@ -3,7 +3,9 @@
 token=${1}
 tag=${2}
 name=${2}
-description=$(echo ${3} | sed -z \'s/\\n/\\\\n/g\') # Escape line breaks to prevent json parsing problems
-release=$(curl -XPOST -H "Authorization:token $token" --data "{\\"tag_name\\": \\"$tag\\", \\"target_commitish\\": \\"master\\", \\"name\\": \\"$name\\", \\"body\\": \\"$description\\", \\"draft\\": false, \\"prerelease\\": true}" https://api.github.com/repos/shayaantx/botdar/releases)
-id=$(echo "$release" | sed -n -e 's/"id":\\ \\([0-9]\\+\\),/\\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+
+description=$(echo "${3}" | sed -z 's/\n/\\n/g') # Escape
+release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/shayaantx/botdar/releases)
+id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+
 curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary "target/botdar-release.jar" https://uploads.github.com/repos/shayaantx/botdar/releases/$id/assets?name=botdar-release.jar
