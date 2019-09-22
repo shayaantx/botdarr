f729800ebef05c659798e728a531b2452a26e119
shayaantx <shayaantx@gmail.com>
2019-09-21 23:43:53 -0400

    Update upload-release.sh

--- upload-release.sh
+++ upload-release.sh
@@ -8,6 +8,4 @@ description=$(echo "${3}" | sed -z 's/\n/\\n/g') # Escape
 release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/shayaantx/botdar/releases)
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 
-ls -l
-ls -l target/
-curl -vv -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary "target/botdar-release.jar" https://uploads.github.com/repos/shayaantx/botdar/releases/$id/assets?name=botdar-release.jar
+curl -vv -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @target/botdar-release.jar https://uploads.github.com/repos/shayaantx/botdar/releases/$id/assets?name=botdar-release.jar
