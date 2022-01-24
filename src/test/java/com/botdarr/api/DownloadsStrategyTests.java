4e4be4d71d7ecba933c444eb730edae8ce20af39
shayaantx <5449086+shayaantx@users.noreply.github.com>
2022-01-23 21:59:16 -0500

    Make connection errors during notifications silent (logged only) (#52)
    Also scrub apikey from any future log statements (not aware that it was
    logged prior, but with this new change it would of been without the
    scrub) + Test cases

--- src/test/java/com/botdarr/api/DownloadsStrategyTests.java
+++ src/test/java/com/botdarr/api/DownloadsStrategyTests.java
@@ -3,11 +3,18 @@ package com.botdarr.api;
 import com.botdarr.TestResponse;
 import com.botdarr.clients.ChatClientResponse;
 import com.botdarr.clients.ChatClientResponseBuilder;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import mockit.*;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,11 +68,36 @@ public class DownloadsStrategyTests {
     Assert.assertEquals(expectedInfoResponse, responses.get(0));
   }
 
+  @Test
+  public void getContentDownloads_endpointUnavailable_emptyResponse() {
+    DownloadsStrategy downloadsStrategy = new DownloadsStrategy(api, "", chatClientResponseBuilder, ContentType.MOVIE) {
+      @Override
+      public ChatClientResponse getResponse(JsonElement rawElement) {
+        return null;
+      }
+    };
+    Deencapsulation.setField(downloadsStrategy, "LOGGER", logger);
+    new Expectations(downloadsStrategy) {{
+      api.getApiUrl(""); result = "http://localhost"; times = 2;
+      api.getApiToken(); result = "token1"; times = 1;
+      logger.error("Error trying to connect to http://localhost"); times = 1;
+    }};
+
+    //confirm even tho we failed to connect we don't return error notifications that could flood chat clients
+    Assert.assertTrue(downloadsStrategy.getContentDownloads().isEmpty());
+  }
+
   private DownloadsStrategy getMockDownloadsStrategy(int maxDownloadsToShow) {
     DownloadsStrategy mockDownloadsStrategy = new MockDownloadsStrategy(api, "", chatClientResponseBuilder, ContentType.MOVIE);
     Deencapsulation.setField(mockDownloadsStrategy, "MAX_DOWNLOADS_TO_SHOW", maxDownloadsToShow);
     return  mockDownloadsStrategy;
   }
+
+  @Rule
+  public MockServerRule mockServerRule = new MockServerRule(this);
+
+  @Mocked
+  private Logger logger;
 
   @Injectable
   private Api api;
---
+++ src/test/java/com/botdarr/utilities/Log4jSensitiveDataPatternTests.java
@@ -0,0 +1,39 @@
+package com.botdarr.utilities;
+
+import mockit.Expectations;
+import mockit.Mocked;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Log4jSensitiveDataPatternTests {
+    @Test
+    public void format_noApiKeyInString_noMaskedText() {
+        String input = "http://localhost";
+        LogEventPatternConverter patternConverter = new Log4jSensitiveDataPattern("test", "style");
+        StringBuilder output = new StringBuilder();
+        new Expectations() {{
+           mockedEvent.getMessage().getFormattedMessage(); result = input;
+        }};
+        patternConverter.format(mockedEvent, output);
+        //input should remain unchanged
+        Assert.assertEquals(input, output.toString());
+    }
+
+    @Test
+    public void format_apiKeyInString_noMaskedText() {
+        String input = "http://localhost?apikey=fdskjkjfd";
+        LogEventPatternConverter patternConverter = new Log4jSensitiveDataPattern("test", "style");
+        StringBuilder output = new StringBuilder();
+        new Expectations() {{
+            mockedEvent.getMessage().getFormattedMessage(); result = input;
+        }};
+        patternConverter.format(mockedEvent, output);
+        //input should remain unchanged
+        Assert.assertEquals("http://localhost?apikey=****", output.toString());
+    }
+
+    @Mocked
+    private Log4jLogEvent mockedEvent;
+}
