c4c04c0e89c7d44e1a2b1b4fc9d004e1d6eb6a1d
shayaantx <5449086+shayaantx@users.noreply.github.com>
2022-05-14 14:01:43 -0400

    more

--- src/main/java/com/botdarr/clients/discord/DiscordChatClient.java
+++ src/main/java/com/botdarr/clients/discord/DiscordChatClient.java
@@ -6,6 +6,7 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.Sets;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.apache.logging.log4j.LogManager;
 
 import java.util.*;
 import java.util.List;
@@ -25,18 +26,26 @@ public class DiscordChatClient implements ChatClient<DiscordResponse> {
   }
 
   public void sendMessage(DiscordResponse chatClientResponse, String channelName) {
-    sendMessages(channel -> channel.sendMessage(chatClientResponse.getMessage()).queue(), channelName);
+    sendMessages(channel -> {
+      LogManager.getLogger("com.botdarr.clients.discord").debug("Attempting to queue message to channel " + channelName);
+      channel.sendMessage(chatClientResponse.getMessage()).queue();
+      LogManager.getLogger("com.botdarr.clients.discord").debug("Finished queue message to channel " + channelName);
+    }, channelName);
   }
 
   private void sendMessages(MessageSender messageSender, String channelName) {
+    LogManager.getLogger("com.botdarr.clients.discord").debug("Attempting to message to channel " + channelName);
     Set<String> supportedDiscordChannels = Sets.newHashSet(Splitter.on(',').trimResults().split(Config.getProperty(Config.Constants.DISCORD_CHANNELS)));
     for (TextChannel textChannel : jda.getTextChannels()) {
       if (!supportedDiscordChannels.contains(textChannel.getName())) {
+        LogManager.getLogger("com.botdarr.clients.discord").debug("Channel  " + textChannel.getName() + " not supported");
         continue;
       }
       if (channelName != null && !channelName.equalsIgnoreCase(textChannel.getName())) {
+        LogManager.getLogger("com.botdarr.clients.discord").debug("Channel  " + textChannel.getName() + " and " + channelName + " dont match");
         continue;
       }
+      LogManager.getLogger("com.botdarr.clients.discord").debug("Sending to channel " + textChannel.getName());
       messageSender.send(textChannel);
     }
   }
